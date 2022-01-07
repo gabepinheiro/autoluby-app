17730b7f41977c7f915f3478adf871ae759906e4
Gabe Pinheiro <gabrielpsilvas@live.com>
2022-01-07 19:31:26 -0300

    add handle logout on Header layout page

--- src/layout/header/index.tsx
+++ src/layout/header/index.tsx
@@ -1,10 +1,20 @@
+import { useNavigate } from 'react-router-dom'
+import { useUserAuth } from 'contexts/user-auth'
 import { Logo, Button } from 'ui'
+import * as S from './styles'
+
 import { ReactComponent as LogOutIcon } from 'assets/imgs/log-out.svg'
 import { ReactComponent as SearchIcon } from 'assets/imgs/search.svg'
 
-import * as S from './styles'
-
 export const Header = () => {
+  const { logout } = useUserAuth()
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    logout()
+    navigate('/')
+  }
+
   return (
     <S.Wrapper>
       <S.Container>
@@ -13,7 +23,12 @@ export const Header = () => {
           <S.Input type='text' />
           <SearchIcon />
         </S.InputWrapper>
-        <Button withIcon={<LogOutIcon />}>Sair</Button>
+        <Button
+          withIcon={<LogOutIcon />}
+          onClick={handleLogout}
+        >
+          Sair
+        </Button>
       </S.Container>
     </S.Wrapper>
   )
